72671a1faeb0540c0f0f7695f54c45359166e216
Finlay Nathan <nif3100@gmail.com>
2025-05-19 16:18:12 -0700

    Better tests

--- GrocerEaseTests/GrocerEaseTests.swift
+++ GrocerEaseTests/GrocerEaseTests.swift
@@ -13,7 +13,7 @@ struct GrocerEaseTests {
     @Test func testSearchSafeway() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
         let store = GroceryStore(id: "799", brand: "Safeway", source: .albertsons)
-        let results = try await GrocerEase.SafewayScraper.shared.searchItems(query: "chips", store: store)
+        let results = try await store.source.scraper.shared.searchItems(query: "chips", store: store)
         #expect(!results.isEmpty)
         
     }
@@ -21,7 +21,7 @@ struct GrocerEaseTests {
     @Test func testSearchTraderJoes() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
         let store = GroceryStore(id: "193", brand: "Trader Joe's", source: .traderjoes)
-        let results = try await GrocerEase.TraderJoesScraper.shared.searchItems(query: "chips", store: store)
+        let results = try await store.source.scraper.shared.searchItems(query: "chips", store: store)
         print(results)
         #expect(!results.isEmpty)
     }
