783a925202070952ede617df873f21e663f1f21e
arushityagi29 <arushityagi29@gmail.com>
2025-05-19 18:41:12 -0700

    tinygraphql

--- GrocerEaseTests/GrocerEaseTests.swift
+++ GrocerEaseTests/GrocerEaseTests.swift
@@ -7,6 +7,8 @@
 
 import Testing
 @testable import GrocerEase
+import Foundation
+import TinyGraphQL
 
 struct GrocerEaseTests {
 
@@ -24,6 +26,71 @@ struct GrocerEaseTests {
         let results = try await store.source.scraper.shared.searchItems(query: "chips", store: store)
         print(results)
         #expect(!results.isEmpty)
+    }
+    
+    @Test func testGraphQl() async throws {
+        let originalquery = """
+            query SearchProducts(
+              $search,
+              $pageSize,
+              $currentPage,
+              $storeCode,
+              $availability,
+              $published
+            ) {
+              products(
+                search: $search
+                filter: {
+                  store_code: {eq: $storeCode},
+                  published: {eq: $published},
+                  availability: {match: $availability}
+                }
+                pageSize: $pageSize
+                currentPage: $currentPage
+              ) {
+                items {
+                  sku
+                  name
+                  retail_price
+                  primary_image
+                }
+                total_count
+              }
+            }
+            """
+            
+        let variables: [String : String] = ["storeCode": "232", "availability": "1", "published": "1", "search": "bagel", "currentPage": "1", "pageSize": "15"]
+        
+        let graphQL = GraphQL(
+            url: URL(string: "https://www.traderjoes.com/api/graphql")!,
+            headers: ["Content-Type": "application/json"]
+        )
+        
+        let query = Query("SearchProducts", variables) {
+            Mutation("products", ["search": ]){
+                Field("filter"){
+                    "store_code"
+                    "published"
+                    "availability"
+                }
+                "pageSize"
+                "currentPage"
+            }
+            Field("items"){
+                "sku"
+                "name"
+                "retail_price"
+                "primary_image"
+            }
+            "total_count"
+        }
+
+        let urlRequest = graphQL.request(for: query)
+
+        URLSession.shared.dataTask(with: urlRequest, completionHandler: { data, response, error in
+            print(data)
+        }
+        
     }
 
 }
